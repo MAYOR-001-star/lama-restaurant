a093b4cfc1853a4364c4a4f7ca5d380a40dfc223
MAYOR-001-star <mayorkunabdulazeez01@gmail.com>
2026-07-20 02:33:38 +0100

    fix: resolve orders page rendering errors and hook rules violation

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,5 +1,5 @@
 export type Menu = {
-  id: number;
+  id: string;
   slug: string;
   title: string;
   desc?: string;
@@ -8,10 +8,40 @@ export type Menu = {
 }[];
 
 export type Product = {
-  id: number;
+  id: string;
   title: string;
   desc?: string;
   img?: string;
   price: number;
   options?: { title: string; additionalPrice: number }[];
 };
+
+export type Order = {
+  id: string;
+  userEmail: string;
+  price: number;
+  products: CartItem[];
+  status: string;
+  createdAt: Date;
+  intent_id?: String;
+};
+
+export type CartItem = {
+  id: string;
+  title: string;
+  img?: string;
+  price: number;
+  optionTitle?: string;
+  quantity: number;
+};
+
+export type Cart = {
+  products: CartItem[];
+  totalItems: number;
+  totalPrice: number;
+};
+
+export type ActionTypes = {
+  addToCart:(item:CartItem)=> void;
+  removeFromCart:(item:CartItem)=> void;
+}
--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,28 +1,51 @@
-import NextAuth from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { NextAuthOptions, User, getServerSession } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@next-auth/prisma-adapter"
-import { prisma } from "@/utils/connect";
+import { prisma } from "./connect";
 
-export const authOptions = {
+declare module "next-auth" {
+  interface Session {
+    user: User & {
+      isAdmin: Boolean;
+    };
+  }
+}
+declare module "next-auth/jwt" {
+  interface JWT {
+    isAdmin: Boolean;
+  }
+}
+
+export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   session: {
     strategy: "jwt",
   },
   providers: [
     GoogleProvider({
+      // clientId: process.env.GOOGLE_ID as string,
+      // clientSecret: process.env.GOOGLE_SECRET as string,
       clientId: process.env.AUTH_GOOGLE_ID!,
       clientSecret: process.env.AUTH_GOOGLE_SECRET!,
-      authorization: {
-        params: {
-          prompt: "consent",
-          access_type: "offline",
-          response_type: "code",
-        },
-      },
     }),
   ],
-  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async session({ token, session }) {
+      if (token) {
+        session.user.isAdmin = token.isAdmin;
+      }
+      return session;
+    },
+    async jwt({ token }) {
+      const userInDb = await prisma.user.findUnique({
+        where: {
+          email: token.email!,
+        },
+      });
+      token.isAdmin = userInDb?.isAdmin!;
+      return token;
+    },
+  },
 };
 
-export default NextAuth(authOptions);
-
+export const getAuthSession = () => getServerSession(authOptions);
